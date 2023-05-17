4e8435720a9c52565bf841121a603d19cdec931f
zeethecoder <zion_raymond@hotmail.com>
2023-05-17 09:56:55 -0700

    goin hard

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,20 @@
 /*=============== SHOW MENU ===============*/
 const navMenu = document.getElementById('nav-menu'),
-      navToggle = document.getElementById('nav-toggle'),
-      navClose = document.getElementById('nav-close')
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
-if(navToggle){
-    navToggle.addEventListener('click', () =>{
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
         navMenu.classList.add('show-menu')
     })
 }
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
-if(navClose){
-    navClose.addEventListener('click', () =>{
+if (navClose) {
+    navClose.addEventListener('click', () => {
         navMenu.classList.remove('show-menu')
     })
 }
@@ -23,7 +23,7 @@ if(navClose){
 /*=============== REMOVE MENU MOBILE ===============*/
 const navLink = document.querySelectorAll('.nav__link')
 
-const linkAction = () =>{
+const linkAction = () => {
     const navMenu = document.getElementById('nav-menu')
     // When we click on each nav__link, we remove the show-menu class
     navMenu.classList.remove('show-menu')
@@ -31,17 +31,17 @@ const linkAction = () =>{
 navLink.forEach(item => item.addEventListener('click', linkAction))
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-const scrollHeader = () =>{
+const scrollHeader = () => {
     const header = document.getElementById('header')
-    const toggle = document.getElementById('nav-toggle') 
+    const toggle = document.getElementById('nav-toggle')
     const logo = document.getElementById('nav-logo')
     // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
-    this.scrollY >= 50 ? header.classList.add('bg-header') 
-                       : header.classList.remove('bg-header')
-    this.scrollY >= 50 ? toggle.classList.add('bg-text') 
-                       : toggle.classList.remove('bg-text')
-    this.scrollY >= 50 ? logo.classList.add('bg-text') 
-                       : logo.classList.remove('bg-text')
+    this.scrollY >= 50 ? header.classList.add('bg-header')
+        : header.classList.remove('bg-header')
+    this.scrollY >= 50 ? toggle.classList.add('bg-text')
+        : toggle.classList.remove('bg-text')
+    this.scrollY >= 50 ? logo.classList.add('bg-text')
+        : logo.classList.remove('bg-text')
 }
 window.addEventListener('scroll', scrollHeader)
 
@@ -51,12 +51,12 @@ const popularSwiper = new Swiper('.popular__content', {
     slidesPerView: 'auto',
     centerdSlides: true,
     loop: true,
-  
-  
+
+
     // Navigation arrows
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
     },
 
     breakpoints: {
@@ -64,19 +64,56 @@ const popularSwiper = new Swiper('.popular__content', {
             centerdSlides: false
         }
     }
-  
-  });
+
+});
 
 /*=============== CHOOSE FAQ ===============*/
 
+// the items in the div text title button
+const faqItems = document.querySelectorAll('.choose__faq-item');
 
-/*=============== SHOW SCROLL UP ===============*/ 
+
+
+// looping through each item  
+faqItems.forEach((item) => {
+    const faqHeader = item.querySelector('.choose__faq-header');
+
+    // add onclick event to the button and title 
+    faqHeader.addEventListener('click', () => {
+
+        const openItem = document.querySelector('.faq-open');
+        // call function
+        toggleItem(item);
+
+        if(openItem && openItem != item) {
+            toggleItem(openItem)
+        }
+    })
+})
+
+
+const toggleItem = (item) => {
+    const faqContent = item.querySelector('.choose__faq-content');
+
+    if (item.classList.contains('faq-open')) {
+        faqContent.removeAttribute('style');
+        item.classList.remove('faq-open');
+    } else {
+        faqContent.style.height = faqContent.scrollHeight + 'px';
+        item.classList.add('faq-open');
+    }
+
+
+}
+
+
+/*=============== SHOW SCROLL UP ===============*/
 
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
 
-/*=============== DARK LIGHT THEME ===============*/ 
+/*=============== DARK LIGHT THEME ===============*/
 
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
