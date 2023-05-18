a1030c95cc2f39bc93ae5453e89b41a878b7e407
zeethecoder <zion_raymond@hotmail.com>
2023-05-18 15:17:29 -0400

    Snapped at work

--- assets/js/main.js
+++ assets/js/main.js
@@ -85,7 +85,7 @@ faqItems.forEach((item) => {
         // call function
         toggleItem(item);
 
-        if(openItem && openItem != item) {
+        if (openItem && openItem != item) {
             toggleItem(openItem)
         }
     })
@@ -108,10 +108,33 @@ const toggleItem = (item) => {
 
 
 /*=============== SHOW SCROLL UP ===============*/
-
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up');
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+        : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp);
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('')
 
+const scrollActive = () => {
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id'),
+            sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            sectionsClass.classList.add('active-link')
+        } else {
+            sectionsClass.classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
 
 /*=============== DARK LIGHT THEME ===============*/
 
